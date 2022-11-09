1997a880d321e103e748c1bb3e02fc25a9453ee1
Saleh-Alibrahim <s.albrahim@outlook.com>
2022-11-09 15:36:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todoName, setTodoName] = useState('');
+  const MySwal = withReactContent(Swal);
+
+  const addTodo = (e) => {
+    setTodos([...todos, todoName]);
+    setTodoName('');
+    MySwal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Your todo has been added !',
+      showConfirmButton: false,
+      heightAuto: false,
+
+      timer: 1500,
+    });
+  };
+
+  const deleteTodo = (e) => {
+    const id = e.target.id;
+
+    const newTodos = todos.filter((item, index) => {
+      return index != id;
+    });
+
+    setTodos(newTodos);
+    MySwal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Your todo has been deleted !',
+      showConfirmButton: false,
+      heightAuto: false,
+      timer: 1500,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='container '>
+      <h1 className='text-center'>Todo List !</h1>
+      <ul className='list-group mt-3 text-center'>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index} className='list-group-item'>
+              <h5>{todo}</h5>
+              <button
+                onClick={deleteTodo}
+                type='button'
+                id={index}
+                className='btn btn-danger'
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <div className='input-group mt-5'>
+        <input
+          type='text'
+          value={todoName}
+          onChange={(e) => setTodoName(e.target.value)}
+          className='form-control'
+          placeholder='Add your Todo'
+        />
+        <button
+          onClick={addTodo}
+          className='btn btn-secondary'
+          type='button'
+          id='button-addon2'
         >
-          Learn React
-        </a>
-      </header>
+          Add Todo
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
